5c18578ec81857e8b0545a487e4d2feba72a1821
Nawal Nadeem <nawal.nadeem@gmail.com>
2024-08-29 13:09:50 +0500

    Update player.py

--- player.py
+++ player.py
@@ -1,39 +1,29 @@
-from turtle import Turtle, Screen
+from turtle import Turtle
 
-STARTING_POSITION = (0, -230)
+STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
-FINISH_LINE_Y = 230
-
-
-
-
-
-#Asking user for the input
-
+FINISH_LINE = 280
 
 class Player(Turtle):
+
     def __init__(self):
         super().__init__()
-        screen = Screen()
-        screen.setup(500, 400)
-        user_color = screen.textinput("Turtle Color",
-                                    "Choose your turtle's colour. 'Red', 'Yellow' or 'Green'")
         self.shape("turtle")
-        self.color(user_color)
         self.penup()
-        self.go_to_start()
+        self.goto(STARTING_POSITION)
         self.setheading(90)
 
     def go_up(self):
         self.forward(MOVE_DISTANCE)
 
+
     def go_to_start(self):
         self.goto(STARTING_POSITION)
 
     def is_at_finish_line(self):
-        if self.ycor() > FINISH_LINE_Y:
+        if self.ycor() > FINISH_LINE:
             return True
         else:
             return False
 
-
+    #
